c7ab38fee81464c49f3c3cc44fb346401bed1821
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-03-05 15:40:36 -0800

    Very minor tweak yey parking lot band starts now

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {
+          // <img src={logo} className="App-logo" alt="logo" />
+        }
+        <h1>
+          PARKIING LOT BAND AYYYYYYYYYYYYY
+        </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
